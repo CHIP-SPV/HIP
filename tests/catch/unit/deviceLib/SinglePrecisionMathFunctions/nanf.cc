e8d024a90191d44c7cfdcabfc718ae29c80c4954
Paulius Velesko <pvelesko@pglc.io>
2023-04-12 09:41:50 +0000

    adjust nan nanf tests

--- tests/catch/unit/deviceLib/SinglePrecisionMathFunctions/nanf.cc
+++ tests/catch/unit/deviceLib/SinglePrecisionMathFunctions/nanf.cc
@@ -1,2 +1,3 @@
 #include "../device_tests_common.hh"
-GENERATE_KERNEL_FLOAT(nanf, nanf(const_cast<char*>(reinterpret_cast<char*>(a))));
+__device__ const char *tagp;
+GENERATE_KERNEL_FLOAT(nanf, nanf(tagp));
