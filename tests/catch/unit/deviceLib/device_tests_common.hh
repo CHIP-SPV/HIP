3cd27a6ba540506a3ccc59385918de210c457528
Paulius Velesko <pvelesko@pglc.io>
2023-04-18 06:01:45 +0000

    type cast intrinsics tests

--- tests/catch/unit/deviceLib/device_tests_common.hh
+++ tests/catch/unit/deviceLib/device_tests_common.hh
@@ -54,6 +54,57 @@
     HIP_CHECK(hipFree(Outd));                                                                      \
   }
 
+#define GENERATE_KERNEL_LONGLONG(FUNCNAME, FUNC)                                                \
+  __global__ void testKernel_##FUNCNAME(long long int* a) {                                    \
+    FUNC;                                                                                       \
+  }                                                                                             \
+  TEST_CASE("Unit_deviceFunctions_CompileTest_" #FUNCNAME "_longlong") {                       \
+    long long int* Outd;                                                                       \
+    auto res = hipMalloc((void**)&Outd, SIZE);                                                 \
+    REQUIRE(res == hipSuccess);                                                                \
+    hipLaunchKernelGGL(testKernel_##FUNCNAME, dim3(LEN, 1, 1), dim3(1, 1, 1), 0, 0, Outd);    \
+    HIP_CHECK(hipGetLastError());                                                              \
+    res = hipDeviceSynchronize();                                                              \
+    REQUIRE(res == hipSuccess);                                                                \
+    res = hipGetLastError();                                                                   \
+    REQUIRE(res == hipSuccess);                                                                \
+    HIP_CHECK(hipFree(Outd));                                                                  \
+  }
+
+#define GENERATE_KERNEL_UNSIGNED(FUNCNAME, FUNC)                                               \
+  __global__ void testKernel_##FUNCNAME(unsigned int* a) {                                     \
+    FUNC;                                                                                       \
+  }                                                                                             \
+  TEST_CASE("Unit_deviceFunctions_CompileTest_" #FUNCNAME "_unsigned") {                       \
+    unsigned int* Outd;                                                                        \
+    auto res = hipMalloc((void**)&Outd, SIZE);                                                 \
+    REQUIRE(res == hipSuccess);                                                                \
+    hipLaunchKernelGGL(testKernel_##FUNCNAME, dim3(LEN, 1, 1), dim3(1, 1, 1), 0, 0, Outd);    \
+    HIP_CHECK(hipGetLastError());                                                              \
+    res = hipDeviceSynchronize();                                                              \
+    REQUIRE(res == hipSuccess);                                                                \
+    res = hipGetLastError();                                                                   \
+    REQUIRE(res == hipSuccess);                                                                \
+    HIP_CHECK(hipFree(Outd));                                                                  \
+  }
+
+#define GENERATE_KERNEL_ULONGLONG(FUNCNAME, FUNC)                                                \
+  __global__ void testKernel_##FUNCNAME(unsigned long long int* a) {                             \
+    FUNC;                                                                                        \
+  }                                                                                              \
+  TEST_CASE("Unit_deviceFunctions_CompileTest_" #FUNCNAME "_ulonglong") {                       \
+    unsigned long long int* Outd;                                                                \
+    auto res = hipMalloc((void**)&Outd, SIZE);                                                   \
+    REQUIRE(res == hipSuccess);                                                                  \
+    hipLaunchKernelGGL(testKernel_##FUNCNAME, dim3(LEN, 1, 1), dim3(1, 1, 1), 0, 0, Outd);       \
+    HIP_CHECK(hipGetLastError());                                                                \
+    res = hipDeviceSynchronize();                                                                \
+    REQUIRE(res == hipSuccess);                                                                  \
+    res = hipGetLastError();                                                                     \
+    REQUIRE(res == hipSuccess);                                                                  \
+    HIP_CHECK(hipFree(Outd));                                                                    \
+  }
+
 #define GENERATE_KERNEL_ATOMICS(FUNCNAME, TYPE, FUNC)                                                    \
   __global__ void testKernel_##FUNCNAME() {                                                  \
     FUNC;                                                                                          \
