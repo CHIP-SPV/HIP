e8c281609d0ac3025585e5bc9ca9b5df6319f292
Pekka Jääskeläinen <pekka.jaaskelainen@intel.com>
2023-08-07 11:45:30 +0300

    Fix atomic tests to not use a nullptr
    That's undefined/suspicious behavior. At least it should
    be (not explicitly spelled out by the CUDA PM).

--- tests/catch/unit/deviceLib/Atomics/atomicAnd_system.cc
+++ tests/catch/unit/deviceLib/Atomics/atomicAnd_system.cc
@@ -1,4 +1,4 @@
 #include "../device_tests_common.hh"
-GENERATE_KERNEL_ATOMICS(atomicAnd_system_int, int, atomicAdd_system((int*)nullptr, (int)1))
-GENERATE_KERNEL_ATOMICS(atomicAnd_system_unsigned_int, unsigned int, atomicAdd_system((unsigned int*)nullptr, (unsigned int)1))
-GENERATE_KERNEL_ATOMICS(atomicAnd_system_unsigned_long_long, unsigned long long, atomicAdd_system((unsigned long long*)nullptr, (unsigned long long)1))
+GENERATE_KERNEL_ATOMICS(atomicAnd_system_int, int, atomicAdd_system((int*)a, (int)1))
+GENERATE_KERNEL_ATOMICS(atomicAnd_system_unsigned_int, unsigned int, atomicAdd_system((unsigned int*)a, (unsigned int)1))
+GENERATE_KERNEL_ATOMICS(atomicAnd_system_unsigned_long_long, unsigned long long, atomicAdd_system((unsigned long long*)a, (unsigned long long)1))
