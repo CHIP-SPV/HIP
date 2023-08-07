d6697df91406df8248a8e5cf7a70eff504596dc1
Pekka Jääskeläinen <pekka.jaaskelainen@intel.com>
2023-08-07 11:45:30 +0300

    Fix atomic tests to not use a nullptr
    That's undefined/suspicious behavior. At least it should
    be (not explicitly spelled out by the CUDA PM).

--- tests/catch/unit/deviceLib/Atomics/atomicAdd.cc
+++ tests/catch/unit/deviceLib/Atomics/atomicAdd.cc
@@ -1,6 +1,6 @@
 #include "../device_tests_common.hh"
-GENERATE_KERNEL_ATOMICS(atomicAdd_int, int, atomicAdd((int*)nullptr, (int)1))
-GENERATE_KERNEL_ATOMICS(atomicAdd_usigned_int, unsigned int, atomicAdd((unsigned int*)nullptr, (unsigned int)1))
-GENERATE_KERNEL_ATOMICS(atomicAdd_unsigned_long_long, unsigned long long, atomicAdd((unsigned long long*)nullptr, (unsigned long long)1))
-GENERATE_KERNEL_ATOMICS(atomicAdd_float, float, atomicAdd((float*)nullptr, (float)1))
-GENERATE_KERNEL_ATOMICS(atomicAdd_double, double, atomicAdd((double*)nullptr, (double)1))
+GENERATE_KERNEL_ATOMICS(atomicAdd_int, int, atomicAdd((int*)a, (int)1))
+GENERATE_KERNEL_ATOMICS(atomicAdd_usigned_int, unsigned int, atomicAdd((unsigned int*)a, (unsigned int)1))
+GENERATE_KERNEL_ATOMICS(atomicAdd_unsigned_long_long, unsigned long long, atomicAdd((unsigned long long*)a, (unsigned long long)1))
+GENERATE_KERNEL_ATOMICS(atomicAdd_float, float, atomicAdd((float*)a, (float)1))
+GENERATE_KERNEL_ATOMICS(atomicAdd_double, double, atomicAdd((double*)a, (double)1))
--- tests/catch/unit/deviceLib/Atomics/atomicSub.cc
+++ tests/catch/unit/deviceLib/Atomics/atomicSub.cc
@@ -1,3 +1,3 @@
 #include "../device_tests_common.hh"
-GENERATE_KERNEL_ATOMICS(atomicSub_int, int, atomicSub((int*)nullptr, (int)1))
-GENERATE_KERNEL_ATOMICS(atomicSub_usigned_int, unsigned int, atomicSub((unsigned int*)nullptr, (unsigned int)1))
+GENERATE_KERNEL_ATOMICS(atomicSub_int, int, atomicSub((int*)a, (int)1))
+GENERATE_KERNEL_ATOMICS(atomicSub_usigned_int, unsigned int, atomicSub((unsigned int*)a, (unsigned int)1))
